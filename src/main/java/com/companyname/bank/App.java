f8f71f45d263d7bf5fb6ab1431c4e1f3fcdeb1d7
shremore <shremore@IGTEARLVDI214.igatecorp.com>
2016-08-08 06:26:10 -0400

    Done

---
+++ src/main/java/com/companyname/bank/App.java
@@ -0,0 +1,21 @@
+package com.companyname.bank;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	
+	private App(){
+		
+	}
+	
+	/**
+	 * 
+	 * @param args
+	 */
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
